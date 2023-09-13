2045e5bec1466ceda7497951a226cd59967cd077
kunal <kunalshekte24@gmail.com>
2023-09-13 16:25:01 +0530

    Updated signout feature and redux store

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,43 +1,49 @@
-import React, { useEffect } from 'react'
-import Browse from './Browse'
-import Login from './Login'
-import { RouterProvider } from 'react-router-dom'
-import { createBrowserRouter } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
-import { onAuthStateChanged } from 'firebase/auth'
-import { auth } from '../utils/Firebase'
-import { addUser, removeUser } from '../utils/userSlice'
+import { createBrowserRouter } from "react-router-dom";
+import Browse from "./Browse";
+import Login from "./Login";
+import { RouterProvider } from "react-router-dom";
+import { useEffect } from "react";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth } from "../utils/Firebase";
+import { useDispatch } from "react-redux";
+import { addUser, removeUser } from "../utils/userSlice";
 
 const Body = () => {
-    const dispatch = useDispatch()
-    const appRouter = createBrowserRouter([
-        {  
-            path: "/" , 
-            element: <Login/>
-        },
-        {  
-            path: "/Browse" , 
-            element: <Browse/>
-        },
-    ])
+  const dispatch = useDispatch();
 
-    useEffect(()=>{
-        onAuthStateChanged(auth,(user)=>{
-            if(user){
-                const {uid,email,displayName} = user
-                dispatch(addUser({uid:uid,email:email,displayName:displayName}))
-            }
-            else{
-                dispatch(removeUser())
-            }
-        })
-    },[])
+  const appRouter = createBrowserRouter([
+    {
+      path: "/",
+      element: <Login />,
+    },
+    {
+      path: "/browse",
+      element: <Browse />,
+    },
+  ]);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        const { uid, email, displayName, photoURL } = user;
+        dispatch(
+          addUser({
+            uid: uid,
+            email: email,
+            displayName: displayName,
+            photoURL: photoURL,
+          })
+        );
+      } else {
+        dispatch(removeUser());
+      }
+    });
+  }, []);
 
   return (
     <div>
-        <RouterProvider router={appRouter}/>
+      <RouterProvider router={appRouter} />
     </div>
-  )
-}
-
-export default Body
+  );
+};
+export default Body;
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,110 +1,128 @@
-import React, { useState, useRef } from 'react'
-import Header from './Header'
-import {checkValidData} from '../utils/Validate'
-import { signInWithEmailAndPassword } from "firebase/auth";
-import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../utils/Firebase';
-import { useNavigate } from 'react-router-dom';
+import { useState, useRef } from "react";
+import Header from "./Header";
+import { checkValidData } from "../utils/Validate";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  updateProfile,
+} from "firebase/auth";
+import { auth } from "../utils/Firebase";
+import { useNavigate } from "react-router-dom";
 
 const Login = () => {
-  const [isSignIn, setIsSignIn] = useState(true)
-  const [errorMessage, setErrorMessage] = useState()
+  const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorMessage, setErrorMessage] = useState(null);
+  const navigate = useNavigate();
 
+  const name = useRef(null);
+  const email = useRef(null);
+  const password = useRef(null);
 
-  const email = useRef(null)
-  const password = useRef(null)
-  const name = useRef(null)
-  const navigate = useNavigate()
+  const handleButtonClick = () => {
+    const message = checkValidData(email.current.value, password.current.value);
+    setErrorMessage(message);
+    if (message) return;
 
-
-  const handleClickButton = ()=> {
-    const message = checkValidData(email.current.value, password.current.value)
-    setErrorMessage(message)
-
-    if(message) return;
-    
-    if(!isSignIn){
-      createUserWithEmailAndPassword(auth,email.current.value, password.current.value).then((userCredential)=>{
-        const user = userCredential.user;
-        navigate("/Browse")
-      })
-      .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        setErrorMessage(errorCode +"-"+ errorMessage)
-      })
+    if (!isSignInForm) {
+      // Sign Up Logic
+      createUserWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          const user = userCredential.user;
+          updateProfile(user, {
+            displayName: name.current.value,
+            photoURL: "https://avatars.githubusercontent.com/u/12824231?v=4",
+          })
+            .then(() => {
+              navigate("/browse");
+            })
+            .catch((error) => {
+              setErrorMessage(error.message);
+            });
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
+    } else {
+      // Sign In Logic
+      signInWithEmailAndPassword(
+        auth,
+        email.current.value,
+        password.current.value
+      )
+        .then((userCredential) => {
+          // Signed in
+          const user = userCredential.user;
+          console.log(user);
+          navigate("/browse");
+        })
+        .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          setErrorMessage(errorCode + "-" + errorMessage);
+        });
     }
-    else{
-      signInWithEmailAndPassword(auth, email.current.value, password.current.value).then((userCredential) => {
-      
-      const user = userCredential.user;
-      // console.log(user);  
-      navigate("/Browse")
-  })
-  .catch((error) => {
-    setErrorMessage("User Not Found")
-  });
+  };
 
-    }
-  }
-
-  const toggleSignInForm = () =>{
-    setIsSignIn(!isSignIn)
-  }
+  const toggleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
   return (
-  <div>
-    <Header />
+    <div>
+      <Header />
+      <div className="absolute">
+        <img
+          src="https://assets.nflxext.com/ffe/siteui/vlv3/fc164b4b-f085-44ee-bb7f-ec7df8539eff/d23a1608-7d90-4da1-93d6-bae2fe60a69b/IN-en-20230814-popsignuptwoweeks-perspective_alpha_website_large.jpg"
+          alt="logo"
+        />
+      </div>
+      <form
+        onSubmit={(e) => e.preventDefault()}
+        className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80"
+      >
+        <h1 className="font-bold text-3xl py-4">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
 
-    <div className='absolute'>
-      <img 
-      src='https://assets.nflxext.com/ffe/siteui/vlv3/855ed6e2-d9f1-4afd-90da-96023ec747c3/85eb5b91-25ed-4965-ace9-ba8e4a0ead8d/IN-en-20230828-popsignuptwoweeks-perspective_alpha_website_large.jpg'
-      alt='bg'
-      />
+        {!isSignInForm && (
+          <input
+            ref={name}
+            type="text"
+            placeholder="Full Name"
+            className="p-4 my-4 w-full bg-gray-700"
+          />
+        )}
+        <input
+          ref={email}
+          type="text"
+          placeholder="Email Address"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <input
+          ref={password}
+          type="password"
+          placeholder="Password"
+          className="p-4 my-4 w-full bg-gray-700"
+        />
+        <p className="text-red-500 font-bold text-lg py-2">{errorMessage}</p>
+        <button
+          className="p-4 my-6 bg-red-700 w-full rounded-lg"
+          onClick={handleButtonClick}
+        >
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={toggleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix? Sign Up Now"
+            : "Already registered? Sign In Now."}
+        </p>
+      </form>
     </div>
-
-    <form 
-      onSubmit={(e)=>e.preventDefault()}
-      className='absolute w-4/12 p-12 mx-auto my-36 left-0 right-0 bg-black text-white rounded-lg bg-opacity-75'
-    >
-      <h1 className='font-bold text-3xl py-4'>
-        {isSignIn ? "Sign In" :"Sign Up"}
-      </h1>
-
-      {!isSignIn && (
-        <input ref={name}
-        className="my-4 p-4 rounded-lg w-full bg-gray-600" 
-        type='text' 
-        placeholder='Name'
-      />
-      )}
-      <input 
-        ref={email}
-        className="my-4 p-4 rounded-lg w-full bg-gray-600" 
-        type='email' 
-        placeholder='Email'
-      />
-
-      <input 
-        ref={password}
-        className="my-4 p-4 rounded-lg w-full bg-gray-600" 
-        type='password' 
-        placeholder='Password'
-      />
-
-      <button className='my-6 p-4 rounded-lg bg-red-600 w-full' onClick={handleClickButton}>
-        {isSignIn ? "Sign In" :"Sign Up"}
-      </button>
-      
-      <p className='text-red-500 bold'>
-        {errorMessage}
-      </p>
-      <p onClick={toggleSignInForm} className='py-4 cursor-pointer'>
-        {isSignIn ? "New to Netflix? Sign Up Now":"Already Registered? Sign In Now"}
-      </p>
-    </form>
-
-  </div>
-  )
-}
-
-export default Login
+  );
+};
+export default Login;
