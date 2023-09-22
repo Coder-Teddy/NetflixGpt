085366485277b8df4914a77eea1936ac1d7ff8bf
kunal <kunalshekte24@gmail.com>
2023-09-22 19:33:25 +0530

    Created Vide title component

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,27 +1,17 @@
-/* eslint-disable react-hooks/exhaustive-deps */
-import React, { useEffect } from 'react'
 import Header from './Header'
-import { API_OPTIONS } from '../utils/constant'
-import { useDispatch } from 'react-redux'
-import { addNowPlayingMovies } from '../utils/movieSlice'
+import useNowPlayingMovies from '../hooks/useNowPlayingMovies'
+import MainContainer from './MainContainer'
+import SecondaryContainer from './SecondaryContainer'
 
 const Browse = () => {
-  const dispatch = useDispatch()
-  const getNowPlayingMovies = async() =>{
-    const data = await fetch(
-      "https://api.themoviedb.org/3/movie/now_playing", API_OPTIONS
-    )
-    const json = await data.json();
-    console.log(json.results);
-    dispatch(addNowPlayingMovies(json.results))
-  }
-
-  useEffect(()=>{
-    getNowPlayingMovies();
-  },[])
+  useNowPlayingMovies()
 
   return (
-    <Header />
+    <div>
+      <Header />
+      <MainContainer/>
+      <SecondaryContainer/>
+    </div>
   )
 }
 
