65d06dd1dda1d41b39f06dde7c13822db1273497
kunal <kunalshekte24@gmail.com>
2023-09-23 18:30:58 +0530

    create hooks for upcomin, top-rated movie API

--- src/components/SecondaryContainer.js
+++ src/components/SecondaryContainer.js
@@ -1,9 +1,18 @@
 import React from 'react'
+import MovieList from './MovieList'
+import { useSelector } from 'react-redux'
 
 const SecondaryContainer = () => {
-
+  const movies = useSelector((store)=> store.movies)
   return (
-    <div>secondaryContainer</div>
+    <div className='bg-black'>
+      <div className='-mt-52 z-20 pl-12 relative'>
+        <MovieList title = {"Now Playing"} movies={movies.nowPlayingMovies}/>
+        <MovieList title = {"Popular"} movies={movies.popularMovies}/>
+        <MovieList title = {"Top Rated"} movies={movies.topRatedMovies}/>
+        <MovieList title = {"Upcoming"} movies={movies.upcomingMovies}/>
+      </div>
+    </div>
   )
 }
 
--- src/utils/constant.js
+++ src/utils/constant.js
@@ -3,9 +3,11 @@ export const LOGO = "https://cdn.cookielaw.org/logos/dd6b162f-1a32-456a-9cfe-897
 export const AVATAR_URL = "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
 
 export const API_OPTIONS = {
-    method: 'GET',
-    headers: {
-      accept: 'application/json',
-      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTNlMWUyNzVjMDEwMGQ2ZmMyYzAyOTFkNTc0YTgyNiIsInN1YiI6IjY1MDdmZDFlZmEyN2Y0MDBlYjE4MmI0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KhIVXx-rpEX2JR1fy2P803-60hk8rQ3_zQEBTB898tY '
-    }
-  };
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI1MTNlMWUyNzVjMDEwMGQ2ZmMyYzAyOTFkNTc0YTgyNiIsInN1YiI6IjY1MDdmZDFlZmEyN2Y0MDBlYjE4MmI0ZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.KhIVXx-rpEX2JR1fy2P803-60hk8rQ3_zQEBTB898tY '
+  }
+};
+
+export const IMG_CDN_URL = "https://image.tmdb.org/t/p/w500/"
